2d1ddab8e319e1414ae412ad890747838da3004d
Paulina Żurek <paucik94@gmail.com>
2022-02-19 20:03:19 +0100

    Add main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+liczby_podzielne = []
+
+for i in range (0,100):
+    if i % 5 == 0:
+        liczby_podzielne.append(i)
+print(liczby_podzielne)
+
+cubes = [number ** 3 for number in liczby_podzielne]
+
+print(cubes)
